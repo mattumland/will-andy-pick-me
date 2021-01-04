a75d5a6143be91e783077791190a44d035665ce1
Matt Umland <mattumland@gmail.com>
2021-01-04 13:13:47 -0500

    fix input field bug

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ var input = document.querySelector("#question");
 var answerButton = document.querySelector(".get-answer");
 var eightBall = document.querySelector(".eight-ball");
 var responseBox = document.querySelector("#response-box");
+var placeholder = document.querySelector("placeholder");
 
 answerButton.addEventListener('click', function() {
   var question = getQuestion();
@@ -46,6 +47,7 @@ function getRandomIndex(array) {
 
 function changeDisplay(question, answer) {
   eightBall.classList.toggle("hidden");
+  input.value = `Ask your question here!`;
   var newText =
   `<p>"${question}"</p><p>${answer}</p>`
   responseBox.innerHTML = newText;
