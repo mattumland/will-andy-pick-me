e283307105ed0c7af5e681a573f9d05d24b1c8a1
Matt Umland <mattumland@gmail.com>
2021-01-04 13:28:40 -0500

    Add iteration 2 funciton without formatting

--- index.js
+++ index.js
@@ -25,13 +25,20 @@ var input = document.querySelector("#question");
 var answerButton = document.querySelector(".get-answer");
 var eightBall = document.querySelector(".eight-ball");
 var responseBox = document.querySelector("#response-box");
-var placeholder = document.querySelector("placeholder");
+var clearButton = document.querySelector(".clear-active");
+var dummyClear = document.querySelector(".clear-inactive");
 
 answerButton.addEventListener('click', function() {
   var question = getQuestion();
   var answer = getAnswer();
   changeDisplay(question, answer);
 });
+
+clearButton.addEventListener('click', reloadPage);
+
+function reloadPage() {
+  location.reload();
+}
 
 function getQuestion() {
   return input.value;
@@ -47,6 +54,8 @@ function getRandomIndex(array) {
 
 function changeDisplay(question, answer) {
   eightBall.classList.toggle("hidden");
+  dummyClear.classList.toggle("hidden");
+  clearButton.classList.toggle("hidden");
   input.value = `Ask your question here!`;
   var newText =
   `<p>"${question}"</p><p>${answer}</p>`
